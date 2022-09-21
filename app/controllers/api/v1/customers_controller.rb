4e04f6ab9f2f9c50ce3ab62a159d8af2c4cd4860
Bryce Wein <bryce.wein@gmail.com>
2022-09-21 09:28:37 -0400

    Test: add customers index test

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::CustomersController < ApplicationController
   def index
     @customers = Customer.all
 
-    render json: @customers
+    render json: CustomerSerializer.new(@customers)
   end
 
   # GET /customer
--- spec/requests/api/v1/customers_spec.rb
+++ spec/requests/api/v1/customers_spec.rb
@@ -39,5 +39,34 @@ RSpec.describe '/customers', type: :request do
       expect(customer[:attributes]).to have_key :address
       expect(customer[:attributes][:address]).to be_a String
     end
+    it '#index' do
+      customer1 = Customer.create!(first_name: 'Bryce', last_name: 'Wein', email: 'bryce.wein@gmail.com',
+                                   address: '816 shetland drive')
+      customer2 = Customer.create!(first_name: 'Tim', last_name: 'Brewer', email: 'Tim@gmail.com',
+                                   address: '1234 somewhere else VA')
+
+      get '/api/v1/customers'
+
+      expect(response).to be_successful
+
+      parsed_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_body).to have_key(:data)
+      expect(parsed_body[:data]).to be_a Array
+      expect(parsed_body[:data].length).to eq 2
+
+      customer = parsed_body[:data][0]
+
+      expect(customer).to have_key :attributes
+
+      expect(customer[:attributes]).to have_key :first_name
+      expect(customer[:attributes][:first_name]).to be_a String
+      expect(customer[:attributes]).to have_key :last_name
+      expect(customer[:attributes][:last_name]).to be_a String
+      expect(customer[:attributes]).to have_key :email
+      expect(customer[:attributes][:email]).to be_a String
+      expect(customer[:attributes]).to have_key :address
+      expect(customer[:attributes][:address]).to be_a String
+    end
   end
 end
