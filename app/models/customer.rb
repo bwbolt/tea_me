0a3292113b7a7f4678718ea8553397c43e61983c
Bryce Wein <bryce.wein@gmail.com>
2022-09-22 12:41:23 -0400

    Test: add sad paths for customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,8 @@
 class Customer < ApplicationRecord
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
   has_many :subscriptions
   has_many :teas, through: :subscriptions
 end
--- spec/requests/api/v1/customers_spec.rb
+++ spec/requests/api/v1/customers_spec.rb
@@ -77,5 +77,153 @@ RSpec.describe '/customers', type: :request do
       expect(customer[:attributes]).to have_key :address
       expect(customer[:attributes][:address]).to be_a String
     end
+    it '#create (ignores unwanted data)' do
+      info = {
+        "first_name": 'Bryce',
+        "last_name": 'Wein',
+        "email": 'bryce.wein@gmail.com',
+        "address": '816 Shetland Drive Chesapeake Va 23322',
+        "favorite_color": 'pink'
+      }
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      post '/api/v1/customers', headers: headers, params: JSON.generate(info)
+
+      expect(response).to be_successful
+
+      parsed_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_body).to have_key(:data)
+      expect(parsed_body[:data]).to be_a Hash
+
+      customer = parsed_body[:data]
+
+      expect(customer).to have_key :id
+      expect(customer[:id]).to be_a String
+      expect(customer).to have_key :type
+      expect(customer[:type]).to eq('customer')
+
+      expect(customer).to have_key :relationships
+      expect(customer[:relationships]).to be_a Hash
+
+      expect(customer[:relationships]).to have_key :subscriptions
+      expect(customer[:relationships][:subscriptions]).to be_a Hash
+
+      expect(customer[:relationships][:subscriptions]).to have_key :data
+      expect(customer[:relationships][:subscriptions][:data]).to be_a Array
+
+      expect(customer).to have_key :attributes
+
+      expect(customer[:attributes]).to have_key :first_name
+      expect(customer[:attributes][:first_name]).to be_a String
+      expect(customer[:attributes]).to have_key :last_name
+      expect(customer[:attributes][:last_name]).to be_a String
+      expect(customer[:attributes]).to have_key :email
+      expect(customer[:attributes][:email]).to be_a String
+      expect(customer[:attributes]).to have_key :address
+      expect(customer[:attributes][:address]).to be_a String
+
+      expect(customer[:attributes]).to_not have_key :favorite_color
+    end
+  end
+
+  describe 'sad paths' do
+    describe '#create' do
+      it 'already existing email' do
+        info = {
+          "first_name": 'Bryce',
+          "last_name": 'Wein',
+          "email": 'bryce.wein@gmail.com',
+          "address": '816 Shetland Drive Chesapeake Va 23322'
+        }
+        Customer.create!(info)
+
+        info_attempt = {
+          "first_name": 'Rryce',
+          "last_name": 'Rein',
+          "email": 'bryce.wein@gmail.com',
+          "address": '818 Shetland Drive Chesapeake Va 23322'
+        }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post '/api/v1/customers', headers: headers, params: JSON.generate(info_attempt)
+
+        expect(response).to_not be_successful
+
+        parsed_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_body).to eq({ email: ['has already been taken'] })
+      end
+      it 'missing data (first_name)' do
+        info = {
+          "last_name": 'Wein',
+          "email": 'bryce.wein@gmail.com',
+          "address": '816 Shetland Drive Chesapeake Va 23322'
+        }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post '/api/v1/customers', headers: headers, params: JSON.generate(info)
+
+        expect(response).to_not be_successful
+
+        parsed_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_body).to eq({ first_name: ["can't be blank"] })
+      end
+      it 'missing data (last_name)' do
+        info = {
+          "first_name": 'Bryce',
+          "email": 'bryce.wein@gmail.com',
+          "address": '816 Shetland Drive Chesapeake Va 23322'
+        }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post '/api/v1/customers', headers: headers, params: JSON.generate(info)
+
+        expect(response).to_not be_successful
+
+        parsed_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_body).to eq({ last_name: ["can't be blank"] })
+      end
+      it 'missing data (email)' do
+        info = {
+          "first_name": 'Bryce',
+          "last_name": 'Wein',
+          "address": '816 Shetland Drive Chesapeake Va 23322'
+        }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post '/api/v1/customers', headers: headers, params: JSON.generate(info)
+
+        expect(response).to_not be_successful
+
+        parsed_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_body).to eq({ email: ["can't be blank"] })
+      end
+      it 'missing data (address)' do
+        info = {
+          "first_name": 'Bryce',
+          "last_name": 'Wein',
+          "email": 'bryce.wein@gmail.com'
+        }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post '/api/v1/customers', headers: headers, params: JSON.generate(info)
+
+        expect(response).to_not be_successful
+
+        parsed_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_body).to eq({ address: ["can't be blank"] })
+      end
+    end
   end
 end
